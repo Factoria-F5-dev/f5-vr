4f13f96bac5eaa464412194cc1161b7b1f4567db
anaaragon88 <ana.aragon88@gmail.com>
2025-05-19 11:05:45 +0200

    fix reset button

--- script.js
+++ script.js
@@ -79,11 +79,11 @@ AFRAME.registerComponent("thumbstick-logging", {
     var cameraRig = document.querySelector("#cameraRig");
 
     if (evt.detail.y > 0.95) {
-      debugtxt.setAttribute("value", "DOWN");
+      //debugtxt.setAttribute("value", "DOWN");
       cameraRig.object3D.translateZ(0.05);
     }
     if (evt.detail.y < -0.95) {
-      debugtxt.setAttribute("value", "UP");
+      //debugtxt.setAttribute("value", "UP");
       cameraRig.object3D.translateZ(-0.05);
     }
     if (evt.detail.x < -0.95) {
